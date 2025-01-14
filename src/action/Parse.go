ea8804209613815ca0ce9bee45005939fd02ca56
eloi-menaud <eloimenaud@proton.me>
2025-01-14 16:08:57 +0100

    wring commit

--- src/action/Parse.go
+++ src/action/Parse.go
@@ -24,15 +24,15 @@ func Parse(revision string, strict bool, target string) error {
 	Log.Verb("git log parsed (%d commits)", len(raw_commits))
 
 
-	var wrong_format_commit_ids []string
+	var wrong_format_commits []utils.RawCommit
 
 	Log.Verb("parsing raw commit to conventional commits :",)
 	var commits []utils.Commit
 	// parsing to conventional commits
-	for _,commit := range raw_commits{
-		commit,err := utils.NewCommit(commit)
+	for _,raw_commit := range raw_commits{
+		commit,err := utils.NewCommit(raw_commit)
 		if err != nil {
-			wrong_format_commit_ids = append(wrong_format_commit_ids, commit.Id)
+			wrong_format_commits = append(wrong_format_commits, raw_commit)
 			Log.Verb("  x %s | wrong format", commit.Id)
 		} else {
 			Log.Verb("  ✓ %s | type : %s", commit.Id, commit.Type)
@@ -41,13 +41,17 @@ func Parse(revision string, strict bool, target string) error {
 	}
 
 
-	if len(wrong_format_commit_ids) != 0 {
-		message := fmt.Sprintf(
-			"following commits are not correctly formatted :\n%s\n\nuse 'c14 parse -spec' to see more about format",
-			strings.Join(wrong_format_commit_ids,"\n"),
-		)
+	if len(wrong_format_commits) != 0 {
+		message := "using strict mode, following commits are not correctly formatted :\n"
+		for _,raw_commit := range wrong_format_commits {
+			message = message + fmt.Sprintf(
+				"[%s]\n%s\n",
+				raw_commit.Id,
+				strings.ReplaceAll(raw_commit.Message, "\n", "\n  "),
+			)
+		}
 		if strict {
-			return fmt.Errorf("using strict mode : %s",message)
+			return fmt.Errorf("%s",message)
 		} else {
 			Log.Info("%s",message)
 		}
